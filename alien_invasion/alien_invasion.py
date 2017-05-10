c164b246876157e3b6515bd4272d9964d033d137
GGZZHH <471992509@qq.com>
2017-05-10 17:53:51 +0800

    射击

--- alien_invasion/alien_invasion.py
+++ alien_invasion/alien_invasion.py
@@ -1,8 +1,8 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
-import sys
 
 import pygame
+from pygame.sprite import Group
 
 from settings import Settings
 from ship import Ship
@@ -20,15 +20,18 @@ def run_game():
     
     # 初始化飞船速度 以及 默认出现的位置
     ship = Ship(ai_settings, screen)
+    # 创建一个用于储存子弹的编组
+    bullets = Group()
     
     # 开始游戏的主循环
     while True:
         # 响应鼠标以及键盘事件
-        gf.check_events(ship)
+        gf.check_events(ai_settings, screen, ship, bullets)
         # 更新飞船位置以及事件
         ship.update()
+        gf.update_bullets(bullets)
         # 更新屏幕显示
-        gf.update_screen(ai_settings, screen, ship)
+        gf.update_screen(ai_settings, screen, ship, bullets)
         
 
 # 初始化游戏并开始主循环
