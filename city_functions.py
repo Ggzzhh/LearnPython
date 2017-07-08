b7e8fcd31a1d77a0024988d9b04bc1c82a508bde
GGZZHH <471992509@qq.com>
2017-07-08 10:12:28 +0800

    None

--- city_functions.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
-
-def city_functions(city, country, population=''):
-	if population == '':
-		return ' '.join([city,country]).title()
-	else:
-		message = 'population ' + population
-		return city.title() + ' ' + country.title() + ' - ' + message
--- my_car.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
-#from car import Car
-from electric_car import ElectricCar, Battery
-
-my_tesla = ElectricCar('tesla', 'model s', 2016)
-my_tesla.battery.get_range()
-my_tesla.battery.upgrade_battery()
-my_tesla.battery.get_range()
